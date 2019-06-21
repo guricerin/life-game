967824a267924e1b287d99d697db44db6b30b64e
guricerin <gurizumi@gmail.com>
2019-06-21 11:32:51 +0900

    add cpp

---
+++ cpp/lifegame/lifegame/lifegame.cpp
@@ -0,0 +1,23 @@
+﻿#include "lifegame.h"
+#include <thread>
+#include <chrono>
+#include <cstdlib>
+
+void console_clear() {
+    std::system("cls");
+}
+
+int main() {
+    constexpr int height = 30;
+    constexpr int width = 40;
+    auto lifegame = LifeGame(height, width);
+    lifegame.init_field();
+
+    while (true) {
+        //console_clear(); // 有効化したらチラつきがひどい
+        lifegame.dump();
+        lifegame = lifegame.next_field();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+	return 0;
+}
